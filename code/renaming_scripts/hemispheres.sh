15b3e177bbc256ea3cb1d7b9926cb93d216e2d30
Tamsin Rogers <tamsinrog@gmail.com>
2025-08-15 14:10:16 -0400

    rename hemispheres

--- code/renaming_scripts/hemispheres.sh
+++ code/renaming_scripts/hemispheres.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 ROOT_FOLDER="/Users/tamsin.rogers/Desktop/github/neuromaps-nhp-prep/share/Inputs"
-OUTPUT_CSV="input_vertices.csv"
+OUTPUT_CSV="input_hemispheres.csv"
 
 # Write CSV header
-echo "Subdirectory,Filename,VertexCount" > "$OUTPUT_CSV"
+echo "Subdirectory,Filename,Hemisphere" > "$OUTPUT_CSV"
 
-# Find all files containing "surface" in filename under ROOT_FOLDER
+# Find all files containing "surf" in filename under ROOT_FOLDER
 find "$ROOT_FOLDER" -type f -name "*surf*" | while read -r filepath; do
     echo "Processing file: $filepath"
     filename=$(basename "$filepath")
     subdir=$(dirname "$filepath" | sed "s|$ROOT_FOLDER/||")
-    vertex_count=$(python3 vertices.py "$filepath")
-    echo "$subdir,$filename,$vertex_count" >> "$OUTPUT_CSV"
+    hemisphere=$(python3 hemispheres.py "$filepath")
+    echo "$subdir,$filename,$hemisphere" >> "$OUTPUT_CSV"
 done
